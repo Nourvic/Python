60c120c9db3715c22e79b7ac9c6ac62c0e1ad77e
Nourvic <nourvic7@gmail.com>
2025-04-24 01:09:08 +0200

    kleines Beispiel fuer einfache Methoden-Anwendung

---
+++ Crash-Course/Grundlagen/Methoden.py
@@ -0,0 +1,12 @@
+# Methoden
+
+#Funktion mit Rückgabewert
+def potenz(n: int, k: int) -> int:
+    return n ** k # ** ist der Exponentenoperator z.B.  n=3 , k=2 => 3^2 = 9
+ 
+# Funktion ohne Rückgabewert:
+def begrüße(name: str, formell: bool = False):
+    if formell:
+        print(f"Hallo {name}, wie geht es Ihnen?")
+    else:
+        print(f"Hallo {name}, wie geht's?")
