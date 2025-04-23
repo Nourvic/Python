61b07738868f8d6f6f05216f6a5250bb2b3bafcd
Nourvic <nourvic7@gmail.com>
2025-04-24 00:26:32 +0200

    "First Project in Python"

---
+++ test.py
@@ -0,0 +1,81 @@
+print("Hallo Nour")
+
+#Hallo-Welt
+
+"""
+
+def beispiel():
+    if True:
+        x=5
+        print(x)
+
+
+"""
+
+# Type Hints
+
+"""
+
+name:str
+gewicht : float
+alter : int = 25
+
+"""
+
+# Kontrollstrukturen
+
+"""
+
+alter =18
+if alter >= 18:
+    print("er/sie ist Volljaehrig")
+else:
+    print("er/sie Minderjaehrig")
+
+for i in range(5):
+    print(i)
+    
+x:int=0
+while alter <35:
+    alter+=1
+    x+=1
+print("dessen Wert ist : ",x)
+
+
+"""
+
+# IF - Statements
+
+"""
+
+note = 3
+if note == 1:
+    print("Sehr gut")
+elif note == 2:
+    print("Gut")
+elif note == 3:
+    print("Befriedigend")
+else:
+    print("Verbesserungsbedarf")
+
+"""
+
+# For-Schleife
+
+"""
+for x in ["a","b","c"]:
+    print(x)
+for y in range(5):
+    print(y)
+"""
+
+# While-Schleife
+
+alter =18
+while alter <=35:
+    alter+=1
+    if alter == 21:
+        break
+    print(alter)
+   
+# Python unterstützt keine do-while-Schleifen!
