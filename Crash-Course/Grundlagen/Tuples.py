63ef7f2568082e9b3c230c0bebc2d7ce994612b6
Nourvic <nourvic7@gmail.com>
2025-05-01 01:09:55 +0200

    "Tuples"

---
+++ Crash-Course/Grundlagen/Tuples.py
@@ -0,0 +1,7 @@
+coordinates=(23,45)
+print(coordinates[0])
+# coordiantes[1] = # Der Inhalt eines Tuples kann nicht mehr geändert werden
+# Man kann den Wert an einem bestimmten Index in einem Tuple nicht verändern
+list_of_tuples=[(2,3),(4,5),(6,7)]
+print(list_of_tuples)
+#list_of_tuples[0][0] =5 # Fehler !!
