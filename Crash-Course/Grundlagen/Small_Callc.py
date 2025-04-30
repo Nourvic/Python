187ee71e5fcd83c087ca707074f08927ad2c1c72
Nourvic <nourvic7@gmail.com>
2025-04-30 18:08:30 +0200

    ""

---
+++ Crash-Course/Grundlagen/Small_Callc.py
@@ -0,0 +1,12 @@
+# Concatation Zahlen werden als strings betrachtet 
+num1 = input("enter first num : ")
+num2 = input("enter sec. num : ")
+res = num1+num2
+print(res)
+
+
+# in Zahlen umwandeln => float oder int 
+num3 = input("enter thired num : ")
+num4 = input("enter fourth num : ")
+res2 = float(num3)+float(num4)
+print(res2)
