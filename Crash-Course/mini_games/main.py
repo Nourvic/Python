45d3ec62c680539257fc242421627ccec2430676
Nourvic <nourvic7@gmail.com>
2025-05-03 16:42:25 +0200

    "Position der Schlange defenieren"

--- Crash-Course/mini_games/main.py
+++ Crash-Course/mini_games/main.py
@@ -4,6 +4,8 @@ import random
 # Initialisierung
 screen = curses.initscr()
 curses.curs_set(0)  # Maus verstecken
+
+#getmaxyx() sie frage die Hoehe und Breite vom Fenster(Terminal) ab. z.B. 40ze, 600sp
 screen_height, screen_width = screen.getmaxyx()
 
 # Neues Fenster
@@ -11,35 +13,48 @@ window = curses.newwin(screen_height, screen_width, 0, 0)
 window.keypad(1) # window erhaelt ein Input von der Tastatur
 window.timeout(100) #delay in ms -> 1000ms = 1 s
 #x und y Koordinate fuer den Kopf des Snakes
-snk-x = screen_width /4 #500 //4 --> 125
-snk-y = screen_height /2 #600 //2 -->300
+snk_x = screen_width /4 #500 //4 --> 125
+snk_y = screen_height /2 #600 //2 -->300
 
 #defenieren des Snakes
 snake = [
-    [snk-y,snk-x],
-    [snk-y,snk-x-1]
-    [snk-y,snk-x-2]
+    [snk_y,snk_x], #Kopf
+    [snk_y,snk_x-1], #Koerperteil-1 (Bauch)
+    [snk_y,snk_x-2] #Koerperteil-2 (Schweif)
 ]
 
 # defenieren eines Kerns
-kern =[screen_height//2,screen_width//2]
-#      [600//2,          400//2] ---> kern[300,200]
+kern =[screen_height//2,screen_width//2] #Position vom Futter !!
+#      [600//2,          400//2] ---> kern[300,200] es wird in der Mitte platziert
 
-# 
-window.addch(kern[0],kern[1],curses.ACS_DIAMOND)
+# addch(y,x,zeichen)-> zeichnet etwas an eine Stelle 
+window.addch(kern[0],kern[1],curses.ACS_DIAMOND) 
 
+# Am Anfang bewegt sich die Schlange nach rechts
 key = curses.KEY_RIGHT
 
+
 while True:
+    #getch() -> liest vom benutzer die naechste gedrueckte Taste
     next_key = window.getch()
-    key= key if next_key == -1 else next_key
-   """"
-    if next_key == -1:
-        key =key
-    else:
-        key = next_key
-   """"
-   if snake[0][0] in [0,screen_height]
+    #wenn keine Taste gedrueckt wurde dann bleibe in derselben Richtung
+    #wenn eine Taste  gedrueckt wurde, benutze die neue Taste
+    key = key if next_key == -1 else next_key
+ #  if next_key == -1: # -1 -> heisst wenn keine Input-angabe erfolgt, dann bleibt key unveraendert
+ #      key =key # key bleibt unveraendert
+ #  else:
+ #     key = next_key
+ 
+ if snake[0][0] in [0,screen_height] or snake[0][1] in [0][screen_width] or 
+ snake[0] in sanke[1:]:
+     curses.endwin() # Fenster schliessen
+     quit() #Programm schliessen
+    
+ new_head = [snake[0][0],snake[0][1]]
+if key == curses.KEY_DOWN:
+    new_head[0] +=1
+if key == curses.KEY_UP:
+    new_head[0] -=1
    
 
 window.refresh()  # Bildschirm aktualisieren
